35ac45787c6dd0adf4992460768cbdd5238fa662
EdwardScizorhands <79757548+EdwardScizorhands@users.noreply.github.com>
2022-01-23 14:32:38 -0500

    localhost, fetch

--- sample.js
+++ sample.js
@@ -4,20 +4,38 @@ function do_stuff() {
     console.log("before");
     
     $.ajax({ type: "GET",
-    	     url: "http://www.example.com/example",
+	     url: "http://localhost:8001/bpb",
     	     success: function(a) {
 		 alert("success");
 	     },
-    	     error: function(a) {
+    	     error: function(a,b,c) {
 		 alert("failure");
 		 console.log(a);
+		 console.log(a.status);
+		 console.log(a.statusText);
 	     },
     	   });
     console.log("after");
 }
 
+function do_fetch() {
+    fetch("http://localhost:8001").then(function(res) {
+    	// res instanceof Response == true.
+    	if (res.ok) {
+    	    console.log(res);
+    	} else {
+    	    console.log("Looks like the response wasn't perfect, got status", res.status);
+    	}
+    }, function(e) {
+    	console.log("Fetch failed!", e);
+    });
+    
+}
+
 console.log(2);
 setTimeout(do_stuff, 1000);
 console.log(3);
+
+setTimeout(do_fetch, 7000;)
 
 	   
