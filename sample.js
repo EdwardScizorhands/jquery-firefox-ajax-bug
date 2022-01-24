7b92efc3d560ad11ec95222f860e5aa29ee69540
EdwardScizorhands <79757548+EdwardScizorhands@users.noreply.github.com>
2022-01-23 19:24:09 -0500

    version that works

--- sample.js
+++ sample.js
@@ -1,10 +1,10 @@
 
 console.log(1);
-function do_stuff() {
+function do_stuff(url) {
     console.log("before");
     
     $.ajax({ type: "GET",
-	     url: "http://localhost:8001/bpb",
+	     url: url,
     	     success: function(a) {
 		 alert("success");
 	     },
@@ -18,24 +18,46 @@ function do_stuff() {
     console.log("after");
 }
 
-function do_fetch() {
-    fetch("http://localhost:8001").then(function(res) {
-    	// res instanceof Response == true.
+function do_fetch(url) {
+
+    fetch(url, { mode:'no-cors'} ).then( function(res) {
     	if (res.ok) {
     	    console.log(res);
     	} else {
     	    console.log("Looks like the response wasn't perfect, got status", res.status);
+	    console.log("res is " + res);
+	    console.log(res);
+	    console.log(res.body);
+	    console.log(res.headers);
     	}
+	
+    })
+    /*
+
     }, function(e) {
     	console.log("Fetch failed!", e);
-    });
-    
+    })
+    */
 }
 
 console.log(2);
-setTimeout(do_stuff, 1000);
+//setTimeout(do_stuff, 1000);
 console.log(3);
 
-setTimeout(do_fetch, 7000;)
+setTimeout(function() { do_stuff("http://www.example.com/index.html")} ,
+	   2000);
+
+
+setTimeout(function() { do_stuff("http://localhost:8001/index.html")} ,
+	   4000);
+
 
 	   
+// The grab of example.com always worked
+
+// localhost
+   //: on chrome, fetch fails, but ajax works.
+   // on firefox, both fail, but fetch does actually make the request.  fetch fails the same way as on chrome
+
+
+// why does fetch fail??
